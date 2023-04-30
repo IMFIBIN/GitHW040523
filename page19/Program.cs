2566be6295fd79da422c4978815e57e9887c6d40
Danila Oreshkin <korobkamy50@yandex.ru>
2023-04-30 20:36:48 +0300

    P19

---
+++ page19/Program.cs
@@ -0,0 +1,26 @@
+﻿int firstNum, endNum;
+string numStr;
+Console.Write("Введите пятизначное число: ");
+numStr = Console.ReadLine();
+firstNum = 0;
+endNum = numStr.Length - 1;
+
+if (endNum == 4)
+{
+    while (firstNum < endNum && Char.IsDigit(numStr[firstNum]) && Char.IsDigit(numStr[endNum]))
+    {
+        if (numStr[firstNum] == numStr[endNum])
+        {
+            firstNum++;
+            endNum = endNum - 1;
+        }
+        else
+        {
+            Console.WriteLine($"Число {numStr} не палиндром");
+            firstNum = 5;
+        }
+    }
+    if (firstNum == 2) Console.WriteLine($"число {numStr} - палиндром");
+    if (firstNum != 2 && firstNum != 5) Console.WriteLine($"{numStr} - не верный ввод");
+}
+else Console.WriteLine("Значение не пятизначное");
